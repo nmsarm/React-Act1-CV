05764f1ccabf1824f6a87994d0ebb8376aeaa3e8
nmsarm <daneah.sarmiento@yahoo.com>
2022-02-22 18:32:57 +0800

    Added Events/Seminars, Certificates

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -6,7 +6,48 @@ class Certificates extends React.Component{
             <div class="header-right pt-2">
                 <h4>CERTIFICATES</h4>
                 <hr className="hr-right" style={{height: 4}}></hr>
-            
+                <ul>
+                    <li>
+                        <b>Project Management Fundamentals</b> - Udemy
+                        <br/>
+                        <i><span>Issued Feb 2022 · No Expiration Date</span></i>
+                        <br/>
+                        <span>Credential ID UC-ebfe039d-3d37-411c-8ae3-8b424610c8aa</span>
+                        <br/> 
+                        <a 
+                            href="https://www.udemy.com/certificate/UC-ebfe039d-3d37-411c-8ae3-8b424610c8aa/"
+                            style={{color: "blue"}}
+                        >
+                            See Credential
+                        </a>
+                    </li>
+                    <li className="pt-2">
+                        <b>DevOps, CI/CD(Continuous Integration/Delivery) for Beginners </b> - Udemy
+                        <br/>
+                        <i><span>Issued Feb 2022 · No Expiration Date</span></i>
+                        <br/>
+                        <span>Credential ID UC-918b73b2-4574-4af2-b671-9d7155ea4403</span>
+                        <br/> 
+                        <a 
+                            href="https://www.udemy.com/certificate/UC-918b73b2-4574-4af2-b671-9d7155ea4403/"
+                            style={{color: "blue"}}
+                        >
+                            See Credential
+                        </a>
+                    </li>
+                    <li className="pt-2"> 
+                        <b> Programming Foundations: Software Testing/QA </b> - LinkedIn
+                        <br/>
+                        <i><span>Issued Jan 2022 · No Expiration Date</span></i>
+                        <br/>
+                        <a 
+                            href="https://drive.google.com/file/d/1SMPjpDUJy7pzkpmLa1tauqp6AkYD0lP3/view?usp=sharing"
+                            style={{color: "blue"}}
+                        >
+                            See Credential
+                        </a>
+                    </li>
+                </ul>
             </div>
         )
     }
--- src/components/Seminars.js
+++ src/components/Seminars.js
@@ -4,9 +4,38 @@ class Seminars extends React.Component{
     render(){
         return(
             <div class="header-right pt-4">
-                <h4>SERMINARS ATTENDED</h4>
+                <h4>EVENTS & SEMINARS ATTENDED</h4>
                 <hr className="hr-right" style={{height: 4}}></hr>
-            
+                <ul>
+                    <li>
+                        <b>2022 Solution Challenge: Info Session and Solution Design Day</b> 
+                        <br/>
+                        <i>Google Developer Student Clubs - PUP Sto. Tomas Branch</i>
+                        <br/>
+                        <span>February 1 and 4, 2022</span>
+                    </li>
+                    <li className="pt-2">
+                        <b>JavaScript Workshop</b> 
+                        <br/>
+                        <i>Computer Science Society - University of Santo Tomas</i>
+                        <br/>
+                        <span>April 14, 2021</span>
+                    </li>
+                    <li className="pt-2">
+                        <b>DLSU Technology Summit 2020</b> 
+                        <br/>
+                        <i>La Salle Computer Society - De La Salle University - Manila</i>
+                        <br/>
+                        <span>November 7, 28, and December 5, 2020</span>
+                    </li>
+                    <li className="pt-2">
+                        <b>DICT-YouthHack Manila Startup Challenge 2018: High School Edition</b> 
+                        <br/>
+                        <i>YouthHack Manila</i>
+                        <br/>
+                        <span>January 27 and 28, 2018</span>
+                    </li>
+                </ul>
             </div>
         )
     }
